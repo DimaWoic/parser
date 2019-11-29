d9113ad4962b8ce8e6599d06d9cb57c5fad5ab4f
Дмитрий <51091906+DimaWoic@users.noreply.github.com>
2019-11-29 14:19:03 +0400

    Add files via upload

--- start_parser.py
+++ start_parser.py
@@ -1,31 +1,34 @@
-from petshop.link_pagination import get_pagination
-from petshop.get_page import get_link, get_product
+from link_pagination import get_pagination
+from get_page import get_link, get_product
 import csv
+import time
 
 link = []
-url ='https://www.petshop.ru/catalog/cats/syxkor/#p2=129,133,69,5,110,12,54'
+url_category ='https://www.petshop.ru/catalog/cats/syxkor/#p2=129,133,69,5,110,12,54'
 
 
 
 
 
-for m in get_pagination(url, link):
-    get_pagination(m, link)
+for pagination in get_pagination(url_category, link):
+    get_pagination(pagination, link)
+
+for url in link:
+    time.sleep(3)
+    p = get_link(url)
+    print(p)
+    for l in p:
+        for k in range(4):
+            t = get_product(l, k)
+            if t == None:
+                pass
+            else:
+                with open('products.csv', 'a', newline='') as csvfile:
+                    writer = csv.writer(csvfile, dialect='excel', delimiter=',')
+                    writer.writerow(['', t['title'], t['description'], t['weight'], t['price'], '', t['image']])
 
 
 
-
-p = get_link(url)
-print(p)
-for l in p:
-    for k in range(4):
-        t=get_product(l, k)
-        if t==None:
-            pass
-        else:
-            with open('products.csv', 'a', newline='') as csvfile:
-                        writer = csv.writer(csvfile, dialect='excel', delimiter=',')
-                        writer.writerow(['', t['title'], t['description'], t['weight'], t['price'], '', t['image']])
 
 
 print('Парсинг окончен')
